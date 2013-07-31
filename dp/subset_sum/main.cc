f5ec9be64dea39b9d3ccd5c727697ffa8fef2ea5
Out Of Memory <oom.github@gmail.com>
2013-07-31 14:42:25 -0600

    subset_sum: improve coding style and add some comments

--- dp/subset_sum/main.cc
+++ dp/subset_sum/main.cc
@@ -12,11 +12,9 @@ int psum( Array const & a )
 {
    int sum = 0;
 
-   for( size_t i=0; i<a.size(); ++i )
-   {
+   for( size_t i = 0; i < a.size(); ++i )
       if( a[i] > 0 )
          sum += a[i];
-   }
 
    return sum;
 }
@@ -26,11 +24,9 @@ int nsum( Array const & a )
 {
    int sum = 0;
 
-   for( size_t i=0; i<a.size(); ++i )
-   {
+   for( size_t i = 0; i < a.size(); ++i )
       if( a[i] < 0 )
          sum += a[i];
-   }
 
    return sum;
 }
@@ -43,37 +39,43 @@ Array subset( Array const & a, int sum )
    if( a.empty() || sum > p || sum < n )
       return Array();
 
+   // The idea is to populate a matrix M where M[i][j] indicates
+   // if there exists a subset of a[0]..a[i] which sum equals j.
+   // Since indices can't be negative, a[i][0] holds value for n, a[i][1] for n + 1, etc.
+   //
+   // An element can take one of three values:
+   //    NoWay   - there is no subset which sum equals j
+   //    SeeNext - there is a subset which sum equals j but it doesn't include the element a[i]
+   //    UseMe   - there is a subset which sum equals j and it includes the element a[i].
    int const NoWay   = 0;
    int const SeeNext = 1;
    int const UseMe   = 2;
 
-   std::vector< Array > m( a.size(), Array(p-n+1, NoWay) );
+   std::vector< Array > m( a.size(), Array(p - n + 1, NoWay) );
 
-   m[0][ a[0]-n ] = UseMe;
+   m[0][ a[0] - n ] = UseMe;
 
-   for( size_t i=1; i<a.size(); ++i )
-   {
-      for( size_t j=0; j<m[i].size(); ++j )
+   for( size_t i = 1; i < a.size(); ++i )
+      for( size_t j = 0; j < m[i].size(); ++j )
       {
          int s = n + j - a[i];
 
          if( a[i] == n + j )
             m[i][j] = UseMe;
-         else if( m[i-1][j] != NoWay )
+         else if( m[i - 1][j] != NoWay )
             m[i][j] = SeeNext;
-         else if( s >= n && s <= p && m[i-1][s-n] != NoWay )
+         else if( s >= n && s <= p && m[i - 1][s - n] != NoWay )
             m[i][j] = UseMe;
       }
-   }
 
    Array ret;
 
    int x = a.size() - 1;
    int y = sum;
 
-   while( m[x][y-n] != NoWay )
+   while( m[x][y - n] != NoWay )
    {
-      if( m[x][y-n] == SeeNext )
+      if( m[x][y - n] == SeeNext )
       {
          --x;
       }
@@ -104,25 +106,21 @@ Array subset_bruteforce( Array const & a, int sum )
 
    size_t count = (1 << a.size());
 
-   for( size_t i=1; i<count; ++i )
+   for( size_t i = 1; i < count; ++i )
    {
       int s = 0;
 
-      for( size_t j=0; j<a.size(); ++j )
-      {
+      for( size_t j = 0; j < a.size(); ++j )
          if( i & (1 << j) )
             s += a[j];
-      }
 
       if( s == sum )
       {
          Array ret;
 
-         for( size_t j=0; j<a.size(); ++j )
-         {
+         for( size_t j = 0; j < a.size(); ++j )
             if( i & (1 << j) )
                ret.push_back( a[j] );
-         }
 
          return ret;
       }
@@ -138,7 +136,7 @@ void init( Array & a, size_t sz )
    int lo = -sz;
    int hi = sz * 2 + 1;
 
-   for( size_t i=0; i<sz; ++i )
+   for( size_t i = 0; i < sz; ++i )
       a[i] = lo + (rand() % hi);
 }
 
@@ -151,7 +149,7 @@ void test( size_t sz )
    int n = nsum( a );
    int p = psum( a );
 
-   for( int i=n-1; i<=p+1; ++i )
+   for( int i = n - 1; i <= p + 1; ++i )
    {
       Array bf = subset_bruteforce( a, i );
       Array sm = subset( a, i );
@@ -171,8 +169,6 @@ int main()
    srand( time(0) );
 
    for( size_t sz = 0; sz < 7; ++sz )
-   {
       for( size_t i = 0; i < 10000; ++i )
          test( sz );
-   }
 }
